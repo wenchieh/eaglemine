4afdd253e41badd35e594b85f186998fbf4a58cf
wenchieh <wenchiehfeng@sina.com>
2019-04-22 01:42:13 +0800

    update

--- demo.sh
+++ demo.sh
@@ -2,15 +2,15 @@
 
 ## EagleMine demo
 echo [DEMO EagleMine] 'run EagleMine ...'
-python run_eaglemine.py output/histogram.out output/node2hcel.out output/hcel2avgfeat.out \
+python run_eaglemine.py example/histogram.out example/node2hcel.out example/hcel2avgfeat.out \
 	                         4 dtmnorm 1 2 2 output
-python run_eaglemine_view.py output/histogram.out output/describe.out output/hcel2label.out \
+python run_eaglemine_view.py examplehistogram.out example/describe.out example/hcel2label.out \
 	                         dtmnorm Hubness Out-degree output
 echo [DEMO EagleMine] 'done!'
 echo
 
-# # WaterLevelTree demo
-#echo [DEMO WaterLevelTree] 'run WaterLevelTree ...'
-#python run_waterleveltree.py output/histogram.out output
-#echo [DEMO WaterLevelTree] 'done!'
-#echo
+## WaterLevelTree demo
+echo [DEMO WaterLevelTree] 'run WaterLevelTree ...'
+python run_waterleveltree.py example/histogram.out output
+echo [DEMO WaterLevelTree] 'done!'
+echo
